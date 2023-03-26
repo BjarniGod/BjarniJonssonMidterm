c65c3c9c6f3561df7fe2385943c539a57db9063b
Bjarni Guðmann <bgjonsson@mail.fhsu.edu>
2023-03-26 01:06:07 -0500

    db fix

--- model/database.php
+++ model/database.php
@@ -9,9 +9,11 @@ try {
     $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    $error_message = "Database Error: ";
+    $error_message .= $e->getMessage();
+    include('view/error.php');
+    exit();
 }
 
 ?>

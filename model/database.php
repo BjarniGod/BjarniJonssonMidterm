f6fe9400254205f72d413f8566e071b3f4e24d73
Bjarni Guðmann <bgjonsson@mail.fhsu.edu>
2023-03-26 00:36:13 -0500

    fix

--- model/database.php
+++ model/database.php
@@ -4,7 +4,7 @@
 $dsn = "mysql:host=tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "yss0tyv6mcmb509y";
 $password = "bgns283jrafawq4y";
-$dbname = "	irix1ca8uw2tw4es";
+$dbname = "irix1ca8uw2tw4es";
 
 try{
 $db = new PDO($dsn, $username, $password, $dbname);

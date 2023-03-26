3e6cec0e1d62f28bfbde0be1561357789bf41eaa
Bjarni Guðmann <bgjonsson@mail.fhsu.edu>
2023-03-26 01:02:22 -0500

    finally right database update

--- model/database.php
+++ model/database.php
@@ -1,34 +1,17 @@
 <?php
-// $dsn = "mysql:host=localhost; dbname=zippyusedautos";
-// $username = 'root';
-
-// $dsn = "mysql:host=tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-// $username = "yss0tyv6mcmb509y";
-// $password = "bgns283jrafawq4y";
-// $dbname = "irix1ca8uw2tw4es";
-
-// try{
-// $db = new PDO($dsn, $username, $password, $dbname);
-
-// } catch (PDOException $e) {
-//     $error_message = "Database Error: ";
-//     $error_message .= $e->getMessage();
-//     include('view/error.php');
-//     exit();
-
-// }
 
 $servername = "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "yss0tyv6mcmb509y";
 $password = "bgns283jrafawq4y";
 $dbname = "irix1ca8uw2tw4es";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-echo "Connected successfully";
+
 ?>

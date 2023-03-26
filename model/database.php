50452553dc54671c95ed41599ce1bc79e6e35727
Bjarni Guðmann <bgjonsson@mail.fhsu.edu>
2023-03-26 00:50:42 -0500

    database update

--- model/database.php
+++ model/database.php
@@ -1,19 +1,34 @@
 <?php
 // $dsn = "mysql:host=localhost; dbname=zippyusedautos";
 // $username = 'root';
-$dsn = "mysql:host=tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+
+// $dsn = "mysql:host=tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+// $username = "yss0tyv6mcmb509y";
+// $password = "bgns283jrafawq4y";
+// $dbname = "irix1ca8uw2tw4es";
+
+// try{
+// $db = new PDO($dsn, $username, $password, $dbname);
+
+// } catch (PDOException $e) {
+//     $error_message = "Database Error: ";
+//     $error_message .= $e->getMessage();
+//     include('view/error.php');
+//     exit();
+
+// }
+
+$servername = "tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $username = "yss0tyv6mcmb509y";
 $password = "bgns283jrafawq4y";
 $dbname = "irix1ca8uw2tw4es";
 
-try{
-$db = new PDO($dsn, $username, $password, $dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-} catch (PDOException $e) {
-    $error_message = "Database Error: ";
-    $error_message .= $e->getMessage();
-    include('view/error.php');
-    exit();
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
